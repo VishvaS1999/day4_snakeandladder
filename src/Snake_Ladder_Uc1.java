4a4bf9234fe5f989338ba0ecc10a6e45de4415b8
Vishva <gvishva2601@gmail.com>
2022-10-26 09:10:09 +0530

    player position is 0

---
+++ src/Snake_Ladder_Uc1.java
@@ -0,0 +1,10 @@
+public class Snake_Ladder_Uc1
+{
+    public static void main(String[] args)
+    {
+        int playerposition=0;
+
+        System.out.println("player start position is :-" +playerposition);
+
+    }
+}
