c73159d8b1677995e2c1c3ea0e1e0e6bbcbbfa13
Vishva <gvishva2601@gmail.com>
2022-10-26 09:12:05 +0530

    Player rolldie between 1 to 6

---
+++ src/SnakeLadder_Uc2.java
@@ -0,0 +1,11 @@
+public class SnakeLadder_Uc2
+{
+    public static void main(String[] args)
+    {
+     int playerposition=0;
+        System.out.println("player Position is :-" +playerposition);
+
+        int rolldie = (int)(Math.floor(Math.random()*10) %7);
+        System.out.println("Player rolldie:-" +rolldie);
+    }
+}
